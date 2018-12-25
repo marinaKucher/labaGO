204d100051304ca9876e3c5e7cb3bc8159d583e3
Kucher Marina <kucher.m16@gmail.com>
2018-12-26 01:50:54 +0300

    some fix  and comment

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"database/sql"
 	"html/template"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"strconv"
 	"sync"
@@ -31,25 +30,22 @@ type Document struct {
 }
 
 func (c *Context) renderHomePage(rw web.ResponseWriter, req *web.Request) {
-	rw.Header().Set("Location", "/docs")
-	rw.WriteHeader(http.StatusTemporaryRedirect)
+	rw.Header().Set("Location", "/docs")         //добавление заголовка на какую стриницу перенаправлять
+	rw.WriteHeader(http.StatusTemporaryRedirect) //добавление специального статуса,чтобы браузер автоматически перенаправил
 }
 
 func (c *Context) getDocList(rw web.ResponseWriter, req *web.Request) {
-	log.Println(0)
-	rows, err := db.Query("SELECT id, name FROM docs;")
+	rows, err := db.Query("SELECT id, name FROM docs;") //запрос к базе данных и проверка ошибок
 	if err != nil {
-		log.Println(1)
 		http.Error(rw, err.Error(), http.StatusInternalServerError)
 	}
-	defer rows.Close()
+	defer rows.Close() //деффер отложенное действие
 
-	names := make(map[int]string)
+	names := make(map[int]string) //создание словаря ключ цифра
 	for rows.Next() {
 		var id int
 		var name string
 		if err := rows.Scan(&id, &name); err != nil {
-			log.Println(5)
 			http.Error(rw, err.Error(), http.StatusInternalServerError)
 		}
 		names[id] = name
@@ -57,7 +53,6 @@ func (c *Context) getDocList(rw web.ResponseWriter, req *web.Request) {
 
 	err = templates.ExecuteTemplate(rw, "list.html", names)
 	if err != nil {
-		log.Println(2)
 		http.Error(rw, err.Error(), http.StatusInternalServerError)
 	}
 }
@@ -74,7 +69,7 @@ func renderDoc(rw web.ResponseWriter, title, data string) {
 	}
 }
 
-func (c *Context) getDoc(rw web.ResponseWriter, req *web.Request) {
+func (c *Context) getDoc(rw web.ResponseWriter, req *web.Request) { //принимает куда пишем ответ и сам запрос
 	id, err := strconv.Atoi(req.PathParams["id"])
 	if err != nil {
 		http.Error(rw, err.Error(), http.StatusInternalServerError)
@@ -87,19 +82,18 @@ func (c *Context) getDoc(rw web.ResponseWriter, req *web.Request) {
 	if isCached {
 		renderDoc(rw, value.Name, string(value.Data))
 	} else {
-		result := db.QueryRow("SELECT name, data FROM docs WHERE id=$1;", id)
-
 		var name string
 		var data []byte
-		err = result.Scan(&name, &data)
+		err := db.QueryRow("SELECT name, data FROM docs WHERE id=$1;", id).Scan(&name, &data) //запрос к баз данных возращающий одну строку
+		//и сканируем из строчки в переменные
 		if err != nil {
 			if err == sql.ErrNoRows {
-				renderDoc(rw, "Такого документа не существует", "Извините :(")
+				renderDoc(rw, "Такого документа не существует", " ")
 			}
 			http.Error(rw, err.Error(), http.StatusInternalServerError)
 		}
 		mutex.Lock()
-		cache[id] = &Document{Name: name,
+		cache[id] = &Document{Name: name, //создается новая структура
 			Data: data}
 		mutex.Unlock()
 
@@ -129,7 +123,7 @@ func (c *Context) deleteDoc(rw web.ResponseWriter, req *web.Request) {
 	}
 
 	rw.Header().Set("Location", "/docs")
-	rw.WriteHeader(http.StatusMovedPermanently)
+	rw.WriteHeader(http.StatusTemporaryRedirect)
 }
 
 func (c *Context) sendDocForm(rw web.ResponseWriter, req *web.Request) {
@@ -146,12 +140,13 @@ func (c *Context) sendDoc(rw web.ResponseWriter, req *web.Request) {
 	}
 	defer file.Close()
 
-	data, err := ioutil.ReadAll(file)
+	data, err := ioutil.ReadAll(file) //прочитать полностью
 	if err != nil {
 		http.Error(rw, err.Error(), http.StatusInternalServerError)
 	}
 
 	_, err = db.Exec("INSERT INTO docs(name, data) VALUES($1, $2);", req.FormValue("file_name"), data)
+	//выполняем запрос , добавляем в таблицу
 	if err != nil {
 		http.Error(rw, err.Error(), http.StatusInternalServerError)
 	}
